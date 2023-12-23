b9644ace6fdf44dd20c755ab271f86ab97822ce7
Federico Carboni <48333714+FedericoCarboni@users.noreply.github.com>
2023-12-23 17:11:15 +0100

    tokenizer

---
+++ test/Tokenizer.test.js
@@ -0,0 +1,38 @@
+import {Tokenizer} from '../lib/Tokenizer.js';
+
+describe('Tokenizer', () => {
+    it('should work', () => {
+        const tokenizer = new Tokenizer(`\
+2e100;
+global_markers = [];
+{};
+activate() ->
+(
+    deactivate();
+    p = player();
+    colors = l(
+        'white','yellow','orange','red',
+        'pink','magenta','purple','blue',
+        'light_blue', 'cyan','green','gray',
+        'light_gray'
+    );
+    global_markers = map(colors,
+        marker = create_marker(null, pos(p), _+'_carpet');
+        entity_event(
+            marker, 'on_tick', '__marker_tick',
+            str(p), _i, length(colors));
+        marker
+    )
+);
+`, {
+            allowComments: true,
+            allowNewLineMarkers: false,
+        });
+        let token;
+        while ((token = tokenizer.nextToken()) !== null) {
+            console.log(token);
+            break;
+        }
+        console.log(tokenizer.errors);
+    });
+});
