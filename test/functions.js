ae9fe0b6d424047b23d87fa6cee9981725b1a593
Federico Carboni <48333714+FedericoCarboni@users.noreply.github.com>
2024-01-13 10:52:52 +0100

    test: function expressions

---
+++ test/functions.js
@@ -0,0 +1,35 @@
+import assert from 'assert';
+import {Parser} from '../lib/Parser.js';
+
+describe('Function expressions', function () {
+    it('parses function expressions', function () {
+        const parser = new Parser('func(,,x,,y,,)');
+        const root = parser.parse();
+        /** @type {import('../lib/Parser.js').FunctionExpression} */
+        const expected = {
+            kind: 'FunctionExpression',
+            params: [
+                {
+                    kind: 'Variable',
+                    name: 'x',
+                    start: {offset: 7, line: 0, character: 7},
+                    end: {offset: 8, line: 0, character: 8},
+                },
+                {
+                    kind: 'Variable',
+                    name: 'y',
+                    start: {offset: 10, line: 0, character: 10},
+                    end: {offset: 11, line: 0, character: 11},
+                },
+            ],
+            name: {
+                value: 'func',
+                start: {offset: 0, line: 0, character: 0},
+                end: {offset: 4, line: 0, character: 4},
+            },
+            start: {offset: 0, line: 0, character: 0},
+            end: {offset: 14, line: 0, character: 14},
+        };
+        assert.deepStrictEqual(root, expected);
+    });
+});
