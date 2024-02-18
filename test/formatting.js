a7936d21f11d90e4964dc5e49064db17b4d21dfd
Federico Carboni <48333714+FedericoCarboni@users.noreply.github.com>
2024-02-18 16:45:04 +0100

    fix: remove formatting

--- test/formatting.js
+++ test/formatting.js
@@ -1,9 +1,18 @@
-import {format} from '../lib/formatting.js';
+// import {format} from '../lib/formatting.js';
 
-describe('formatting', function () {
-    it('formats something', function () {
-        console.log(
-            format("f(x) -> (1+0xa1);true&&!false; 'hello \t\nworld'", {trailingSemicolon: false}),
-        );
-    });
-});
+// describe('formatting', function () {
+//     it('formats something', function () {
+//         console.log(
+//             format("f(x) -> (1+0xa1);true&&!false; 'hello \t\nworld'", {trailingSemicolon: false}),
+//         );
+//     });
+// });
+
+// import {Parser} from '../lib/Parser.js';
+
+// describe('xxx', function () {
+//     it('should work', function () {
+//         const parser = new Parser('__config() -> null;');
+//         parser.parse();
+//     });
+// });
